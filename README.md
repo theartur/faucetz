6bdb6598f1631111bba2f2ce3605c68133e6cb22
n33x <artur.alves@gmail.com>
2015-06-05 01:56:40 +0000

    Hyper Ionic Faucetz is born ;}-

--- README.md
+++ README.md
@@ -1,12 +1,4 @@
 # faucetz
 Nobody can stop an idea whose time has come
 
-TO DO:
-- cookie relevant information (jazzcript style)
- - visited: +new Date
- - $paid: 0
-- enhance layout
- - colors
- - grid
-- anotar quantos bitcoins foram colhidos
-- GUARDAR EM: cookie && fisebase && localstorage
+// soon to be cloud stored ;}-
--- card.js
+++ card.js
@@ -3,30 +3,38 @@ Faucetz.onCardTemplateLoad = function (cardTemplate) {
     
     var container = Faucetz.container = $('<div />');
     var body = $("body");
-    var list = Faucetz.extracted.results;
+    var list = JSON.parse(localStorage.Faucetz) || Faucetz.extracted.results;
     var cardsRendered = 0;
+    
+    function saveEverything() {
+        localStorage.Faucetz = JSON.stringify(list);
+        console.log(JSON.stringify(list), document.cookie);
+    }
 
-    function buildRow(item) {
+    function buildFaucetzCard(item) {
         // {
         //     "paysystem": "direct",
         //     "interval": "60 min",
         //     "reward": "550 satoshi",
-        //     "link": "https://goo.gl/yFNhgp",
-        //     "name": "farmsatoshi",
+        //     "link": "https://goo.gl/jazzcript",
+        //     "name": "jazzcript",
         //     "dirty": "true"
         // }
 
-        var index = cardsRendered++;
         var name = item.name;
-        var payments = item.payments || [0];
-        var totalPayed = payments.reduce(function(a,b){return a+b;});
+        var interval = item.interval;
+        var url = item.link;
+        var index = item.index || cardsRendered++;
+        var payments = item.payments || [];
+        var comments = item.comments || [];
+        var totalPayed = payments.length && payments.reduce(function(a,b){return a+b;});
         var payLast1 = payments[0] || 0;
         var payLast2 = payments[1] || 0;
         var payLast3 = payments[2] || 0;
         var payLast4 = payments[3] || 0;
         var payLast5 = payments[4] || 0;
-        var comments = (item.comments && item.comments.length) || 0;
-        var interval = item.interval;
+        
+        item.index = index;
 
         var itemTemplate = Faucetz.cardTemplate
             .replace("{{ index }}", index)
@@ -34,21 +42,79 @@ Faucetz.onCardTemplateLoad = function (cardTemplate) {
             .replace("{{ totalPayed }}", totalPayed)
             .replace("{{ payLast1 }}", payLast1)
             .replace("{{ payLast2 }}", payLast2)
-            .replace("{{ payLast2 }}", payLast2)
+            .replace("{{ payLast2 }}", payLast3)
             .replace("{{ payLast4 }}", payLast4)
             .replace("{{ payLast5 }}", payLast5)
-            .replace("{{ payments }}", payments)
-            .replace("{{ comments }}", comments)
+            .replace("{{ payments }}", payments.length)
+            .replace("{{ commentsLength }}", comments.length)
             .replace("{{ interval }}", interval)
+            .replace("{{ url }}", url)
         ;
 
-        var row = $(itemTemplate);
+        var faucetzCard = $(itemTemplate);
 
-        row.find(".visit").click(function () {
-            $(this).css("background-color", "#cfb").text("OK");
+        faucetzCard.find(".payed").click(function () {
+            item.payments = item.payments || [];
+
+            var amount = prompt("how many satoshis?")|0;
+            
+            if (amount) {
+                item.payments.unshift(amount);
+                
+                var updatedValues = buildFaucetzCard(item);
+                
+                updatedValues
+                    .find(".payed")
+                    .removeClass("action")
+                    .addClass("action");
+
+                updatedValues.insertAfter(faucetzCard);
+
+                faucetzCard.remove();
+            }
+            
+            saveEverything();
         });
 
-        return row;
+        faucetzCard.find(".comment").click(function () {
+            
+            item.comments = item.comments || [];
+            
+            var comment = prompt("do you want to remember something?");
+            
+            if (comment) {
+                item.comments.push(comment);
+            
+                var updatedValues = buildFaucetzCard(item);
+
+                var commentsAvailable = item.comments.length;
+
+                while (commentsAvailable--) {
+                    comment = "<li>" + item.comments[commentsAvailable] + "</li>";
+                    updatedValues.find(".comments-section").append(comment);
+                }
+
+                updatedValues
+                    .find(".comment")
+                    .css("background-color", "#cfb");
+                
+                updatedValues.insertAfter(faucetzCard);
+                
+                faucetzCard.remove();
+            }
+            
+            saveEverything();
+        });
+
+        faucetzCard.find(".visit").click(function () {
+            $(this)
+                .removeClass("action")
+                .addClass("action");
+            
+            saveEverything();
+        });
+
+        return faucetzCard;
     }
 
     body.css({
@@ -59,17 +125,17 @@ Faucetz.onCardTemplateLoad = function (cardTemplate) {
 
     container.appendTo(body);
 
-    console.log("list: ", list.length, list);
+    console.log((JSON.stringify(list).length/1024)|0, "kb");
 
-    container.css({
-        border: "1px solid #666",
-        width: "95vw",
-        margin: "10vw auto",
-        backgroundColor: "#ddd"
-    });
+//     container.css({
+//         border: "1px solid #666",
+//         width: "95vw",
+//         margin: "10vw auto",
+//         backgroundColor: "#ddd"
+//     });
 
     for (var i = 0; i < list.length; i++) {
-        container.append(buildRow(list[i]));
+        container.append(buildFaucetzCard(list[i]));
     }
 
 };
