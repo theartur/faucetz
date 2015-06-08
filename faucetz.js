37227269f2517676e0114660e53ecbefbcd36a9a
jazzcript <artur.alves@gmail.com>
2015-06-07 23:09:25 -0300

    adaptation FIX for non root deploys

--- faucetz.js
+++ faucetz.js
@@ -177,7 +177,7 @@ Faucetz.getRandomGoldImg = function () {
     var imgUrl = Faucetz.getRandomGoldImg_buffer.shift();
 
     if (location.host == "www.comodominaromundoagora.com.br") {
-        imgUrl = "pijamoney/" + imgUrl;
+        imgUrl = "pijamoney" + imgUrl;
     }
 
 //     imgUrl = Faucetz.getRandomGoldImg_buffer.splice(rnd, 1); // return random from list
