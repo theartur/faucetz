6c84f973b22a7aea431561c66b277cea8264eb3a
Artur Alves <artur.alves@stayfilm.com>
2015-09-30 14:12:15 -0300

    minor fixes and internal etag added for clean faucet list

--- faucetz.js
+++ faucetz.js
@@ -1,141 +1,142 @@
 window.onload = function () {
-    if ( ! localStorage.FaucetzClean) {
-        localStorage.Faucetz = [];
-        localStorage.FaucetzClean = true;
-        console.log("Local lists were cleaned");
-    } else {
-        console.log("Served lists are clean");
-    }
-    
-    $.get("card.html?_=" + Math.random(), Faucetz.onCardTemplateLoad);
+	var internalEtag = 'FaucetzCleanXYZ'; // FaucetzClean(xxx) is internal ETAG
+	if ( ! localStorage[internalEtag]) {
+		localStorage.Faucetz = [];
+		localStorage[internalEtag] = true;
+		console.log("Local lists were cleaned");
+	} else {
+		console.log("Served lists are clean");
+	}
+	
+	$.get("card.html?_=" + Math.random(), Faucetz.onCardTemplateLoad);
 };
 
-    
+	
 Faucetz.cardsRendered = 0;
 Faucetz.buildFaucetzCard = function (item, newCard) {
-    // {
-    //     "paysystem": "direct",
-    //     "interval": "60 min",
-    //     "reward": "550 satoshi",
-    //     "link": "https://goo.gl/jazzcript",
-    //     "name": "jazzcript",
-    //     "dirty": "true"
-    // }
+	// {
+	//     "paysystem": "direct",
+	//     "interval": "60 min",
+	//     "reward": "550 satoshi",
+	//     "link": "https://goo.gl/jazzcript",
+	//     "name": "jazzcript",
+	//     "dirty": "true"
+	// }
 
-    item.user = item.user || {};
-    item.user.payments = item.user.payments || [];
-    item.user.comments = item.user.comments || [];
+	item.user = item.user || {};
+	item.user.payments = item.user.payments || [];
+	item.user.comments = item.user.comments || [];
 
-    var index;
-    if (newCard) {
-        Faucetz.cardsRendered++;
-        index = Faucetz.cardsRendered;
-    } else {
-        index = item.index;
-    }
+	var index;
+	if (newCard) {
+		Faucetz.cardsRendered++;
+		index = Faucetz.cardsRendered;
+	} else {
+		index = item.index;
+	}
 
-    var clean = item.clean ? "clean" : "";
-    var currentStatus = item.clean ? '<i class="icon ion-cash"></i>' : "";
-    var name = item.name;
-    var interval = item.interval;
-    var url = item.link;
-    var payments = item.user.payments || [];
-    var comments = item.user.comments || [];
-    var commentsText = comments.length ? "<li>" + comments.join("</li><li>") + "</li>" : "";
-    var totalPayed = payments.length && payments.reduce(function(a,b){return a+b;});
-    var payLast1 = payments[0] || "-";
-    var payLast2 = payments[1] || "-";
-    var payLast3 = payments[2] || "-";
-    var payLast4 = payments[3] || "-";
-    var payLast5 = payments[4] || "-";
-    var avatar = item.avatar || Faucetz.getRandomGoldImg();
-    var mainImage = item.mainImage || Faucetz.getRandomGoldImg();
+	var clean = item.clean ? "clean" : "";
+	var currentStatus = item.clean ? '<i class="icon ion-cash"></i>' : "";
+	var name = item.name;
+	var interval = item.interval;
+	var url = item.link;
+	var payments = item.user.payments || [];
+	var comments = item.user.comments || [];
+	var commentsText = comments.length ? "<li>" + comments.join("</li><li>") + "</li>" : "";
+	var totalPayed = payments.length && payments.reduce(function(a,b){return a+b;});
+	var payLast1 = payments[0] || "-";
+	var payLast2 = payments[1] || "-";
+	var payLast3 = payments[2] || "-";
+	var payLast4 = payments[3] || "-";
+	var payLast5 = payments[4] || "-";
+	var avatar = item.avatar || Faucetz.getRandomGoldImg();
+	var mainImage = item.mainImage || Faucetz.getRandomGoldImg();
 
-    item.index = index;
-    item.avatar = avatar;
-    item.mainImage = mainImage;
-    
-    var itemTemplate = Faucetz.cardTemplate
-        .replace("{{ index }}", index)
-        .replace("{{ clean }}", clean)
-        .replace("{{ currentStatus }}", currentStatus)
-        .replace("{{ name }}", name)
-        .replace("{{ totalPayed }}", totalPayed)
-        .replace("{{ payLast1 }}", payLast1)
-        .replace("{{ payLast2 }}", payLast2)
-        .replace("{{ payLast2 }}", payLast3)
-        .replace("{{ payLast4 }}", payLast4)
-        .replace("{{ payLast5 }}", payLast5)
-        .replace("{{ payments }}", payments.length)
-        .replace("{{ comments }}", commentsText)
-        .replace("{{ commentsLength }}", comments.length)
-        .replace("{{ interval }}", interval)
-        .replace("{{ url }}", url)
-        .replace("{{ avatar }}", avatar)
-        .replace("{{ mainImage }}", mainImage)
-    ;
+	item.index = index;
+	item.avatar = avatar;
+	item.mainImage = mainImage;
+	
+	var itemTemplate = Faucetz.cardTemplate
+		.replace("{{ index }}", index)
+		.replace("{{ clean }}", clean)
+		.replace("{{ currentStatus }}", currentStatus)
+		.replace("{{ name }}", name)
+		.replace("{{ totalPayed }}", totalPayed)
+		.replace("{{ payLast1 }}", payLast1)
+		.replace("{{ payLast2 }}", payLast2)
+		.replace("{{ payLast3 }}", payLast3)
+		.replace("{{ payLast4 }}", payLast4)
+		.replace("{{ payLast5 }}", payLast5)
+		.replace("{{ payments }}", payments.length)
+		.replace("{{ comments }}", commentsText)
+		.replace("{{ commentsLength }}", comments.length)
+		.replace("{{ interval }}", interval)
+		.replace("{{ url }}", url)
+		.replace("{{ avatar }}", avatar)
+		.replace("{{ mainImage }}", mainImage)
+	;
 
-    var faucetzCard = $(itemTemplate);
+	var faucetzCard = $(itemTemplate);
 
-    faucetzCard.find(".payed").click(function () {
+	faucetzCard.find(".payed").click(function () {
 
-        var amount = prompt("how many satoshis?")|0;
+		var amount = prompt("how many satoshis?")|0;
 
-        if (amount) {
-            item.user.payments.unshift(amount);
+		if (amount) {
+			item.user.payments.unshift(amount);
 
-            var updatedValues = Faucetz.buildFaucetzCard(item);
+			var updatedValues = Faucetz.buildFaucetzCard(item);
 
-            updatedValues
-                .find(".payed")
-                .removeClass("action")
-                .addClass("action");
+			updatedValues
+				.find(".payed")
+				.removeClass("action")
+				.addClass("action");
 
-            updatedValues.insertAfter(faucetzCard);
+			updatedValues.insertAfter(faucetzCard);
 
-            faucetzCard.remove();
-        }
+			faucetzCard.remove();
+		}
 
-        Faucetz.saveEverything("payment", amount);
-    });
+		Faucetz.saveEverything("payment", amount);
+	});
 
-    faucetzCard.find(".comment").click(function () {
+	faucetzCard.find(".comment").click(function () {
 
-        var comment = prompt("do you want to say something?");
+		var comment = prompt("do you want to say something?");
 
-        if (comment) {
-            item.user.comments.push(comment);
+		if (comment) {
+			item.user.comments.push(comment);
 
-            var updatedValues = Faucetz.buildFaucetzCard(item);
+			var updatedValues = Faucetz.buildFaucetzCard(item);
 
-            var commentsAvailable = item.user.comments.length;
+			var commentsAvailable = item.user.comments.length;
 
-            while (commentsAvailable--) {
-                comment = "<li>" + item.user.comments[commentsAvailable] + "</li>";
-                //                     updatedValues.find(".comments-section").append(comment);
-            }
+			while (commentsAvailable--) {
+				comment = "<li>" + item.user.comments[commentsAvailable] + "</li>";
+				//                     updatedValues.find(".comments-section").append(comment);
+			}
 
-            updatedValues
-                .find(".comment")
-                .css("background-color", "#cfb");
+			updatedValues
+				.find(".comment")
+				.css("background-color", "#cfb");
 
-            updatedValues.insertAfter(faucetzCard);
+			updatedValues.insertAfter(faucetzCard);
 
-            faucetzCard.remove();
-        }
+			faucetzCard.remove();
+		}
 
-        Faucetz.saveEverything("comment", comment);
-    });
+		Faucetz.saveEverything("comment", comment);
+	});
 
-    faucetzCard.find(".visit").click(function () {
-        $(this)
-            .removeClass("action")
-            .addClass("action");
+	faucetzCard.find(".visit").click(function () {
+		$(this)
+			.removeClass("action")
+			.addClass("action");
 
-        Faucetz.saveEverything("visit", $(this).attr("href"));
-    });
+		Faucetz.saveEverything("visit", $(this).attr("href"));
+	});
 
-    return faucetzCard;
+	return faucetzCard;
 };
 
 Faucetz.filterCleanFaucetz = function (list) {
@@ -161,38 +162,38 @@ Faucetz.filterCleanFaucetz = function (list) {
 };
 
 Faucetz.random = function (min, max) {
-      return Math.floor(Math.random() * (max - min)) + min;
-    };
+	return Math.floor(Math.random() * (max - min)) + min;
+};
 
 Faucetz.getRandomGoldImg = function () {
-    
-    Faucetz.getRandomGoldImg_buffer = Faucetz.getRandomGoldImg_buffer || Faucetz.GoldImgs.slice(0);
-    
-    if (Faucetz.getRandomGoldImg_buffer.length < 1) {
-        Faucetz.getRandomGoldImg_buffer = Faucetz.GoldImgs.slice(0);
-    }
-    
-    var rnd = Faucetz.random(0, Faucetz.getRandomGoldImg_buffer.length);
+	
+	Faucetz.getRandomGoldImg_buffer = Faucetz.getRandomGoldImg_buffer || Faucetz.GoldImgs.slice(0);
+	
+	if (Faucetz.getRandomGoldImg_buffer.length < 1) {
+		Faucetz.getRandomGoldImg_buffer = Faucetz.GoldImgs.slice(0);
+	}
+	
+	var rnd = Faucetz.random(0, Faucetz.getRandomGoldImg_buffer.length);
 
-    var imgUrl = Faucetz.getRandomGoldImg_buffer.shift();
+	var imgUrl = Faucetz.getRandomGoldImg_buffer.shift();
 
 //     imgUrl = Faucetz.getRandomGoldImg_buffer.splice(rnd, 1); // return random from list
-    return imgUrl; // return ordered
+	return imgUrl; // return ordered
 };
 
 Faucetz.rankByReward = function (list) {
-    return list.sort(function (a, b) {
-        return parseInt(b.reward, 10) - parseInt(a.reward, 10);
-    });
+	return list.sort(function (a, b) {
+		return parseInt(b.reward, 10) - parseInt(a.reward, 10);
+	});
 };
 
 Faucetz.cloudLog = new Firebase('https://pijamoney.firebaseio-demo.com/');
 
 Faucetz.saveEverything = function (event, value) {
-    var list = Faucetz.list;
-    localStorage.Faucetz = JSON.stringify(list);
+	var list = Faucetz.list;
+	localStorage.Faucetz = JSON.stringify(list);
 
-    Faucetz.cloudLog.push({user:navigator.userAgent, event:event, value:value});
+	Faucetz.cloudLog.push({user:navigator.userAgent, event:event, value:value});
 
-    console.log("Cache updated", localStorage.Faucetz.length/1024|0, "k");
+	console.log("Cache updated", localStorage.Faucetz.length/1024|0, "k");
 };
