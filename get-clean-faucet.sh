68a059d63626d65938e69b3619e89c7291a8fd4a
jazzcript <artur.alves@gmail.com>
2016-11-02 14:53:29 -0700

    clean

--- get-clean-faucet.sh
+++ get-clean-faucet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -O lecture-glass.codio.io/faucetz-list-clean-btc.js
-curl -O lecture-glass.codio.io/faucetz-list-clean-dash.js
-curl -O lecture-glass.codio.io/faucetz-list-clean-lite.js
-curl -O lecture-glass.codio.io/faucetz-list-clean-doge.js
+curl -O minimum-optic.codio.io/faucetz-list-clean-btc.js
+curl -O minimum-optic.codio.io/faucetz-list-clean-dash.js
+curl -O minimum-optic.codio.io/faucetz-list-clean-lite.js
+curl -O minimum-optic.codio.io/faucetz-list-clean-doge.js
